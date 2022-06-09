299ea4bcd0f6440aab9b5dc137d214ce925a78d0
joaofeliperutowitsch <jfrutowitsch@gmail.com>
2022-06-09 19:20:51 -0300

    writing javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+var $nome = 'felipe';
+let idade = 20;
+let ano = 2022 - idade
+let soma = idade - ano
+let templateString = "Estou mexendo no comsole.log";
+let quartaFeira = true
+
+console.log (soma)
+console.log ("Estou mexendo no console.log")
+console.log (templateString)
+console.log (quartaFeira)
+console.log (typeof quartaFeira)
