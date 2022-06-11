4cf2e087555414ab075629f17a35e029bea3ff5e
joaofeliperutowitsch <jfrutowitsch@gmail.com>
2022-06-11 02:11:49 -0300

    Organizing and finalizing javascript

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let nome = "felipe"
 let aniversario = 40
 let ano = 2022 - aniversario
-console.log (data)
-let soma = idade + ano
+console.log (ano)
+let soma = aniversario + ano
 console.log (soma)
-let templatestring = `Estou mechendo no console.log`;
-console.log (`Estou mechendo no console.log`)
+console.log ("Estou mechendo no console.log")
 let quartaFeira = true
-console.log (quartaFeira)
 console.log (typeof quartaFeira)
